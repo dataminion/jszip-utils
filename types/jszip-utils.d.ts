9503fe1606843d8e7adcb4778422b783ab9ed30e
Austin <leahy@apache.org>
2023-12-19 17:11:20 -0600

    added types

---
+++ types/jszip-utils.d.ts
@@ -0,0 +1,12 @@
+// types/jszip-utils.d.ts
+
+declare module JSZipUtils {
+    // Add your type declarations here
+    // For example:
+    export function createStandardXHR();
+
+    export function createActiveXHR();
+
+    export function getBinaryContent(path, options);
+    export const createXHR: string;
+  }
